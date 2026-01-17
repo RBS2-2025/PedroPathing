ff26144e88bfc56a080e2d584c37ea455ca36ed4
StarB_ <uve0508@naver.com>
2026-01-17 19:40:16 +0900

    Vector2d

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Vector2d.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+public class Vector2d {
+    public double x,y;
+    public Vector2d(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+
+}
