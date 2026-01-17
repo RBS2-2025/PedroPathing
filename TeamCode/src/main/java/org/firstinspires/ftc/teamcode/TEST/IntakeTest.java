e99f55e01b296e91da1e6b4ce927b0a2a4e68b46
StarB_ <uve0508@naver.com>
2026-01-17 20:25:00 +0900

    Input Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TEST/IntakeTest.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.TEST;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "IntakeTest")
+public class IntakeTest extends LinearOpMode {
+    DcMotor intake;
+    boolean intakePressed = false;
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        if (opModeIsActive()) {
+            intake = hardwareMap.dcMotor.get("IntakeDc");
+
+            // Pre-run
+            while (opModeIsActive()) {
+                // OpMode loop
+                if(gamepad1.a && !intakePressed){
+                    intake.setPower(0.7);
+                }
+                if(gamepad1.b && !intakePressed){
+                    intake.setPower(1);
+                }
+                if (intakePressed && !gamepad1.a && !gamepad1.b){
+                    intake.setPower(0);
+                }
+            }
+        }
+    }
+}
